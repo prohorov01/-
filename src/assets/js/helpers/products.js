a687a033f4fe91e9e6c38f0bf97cdb4f83984256
prohorov01 <alexprohorov01@gmail.com>
2023-02-18 11:51:49 +0200

    Initial commit

---
+++ src/assets/js/helpers/products.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper("#solutions-swiper", {
+	loop: true,
+
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
---
+++ src/assets/js/index.js
@@ -0,0 +1,5 @@
+import "bootstrap";
+import "./helpers/focus-visible";
+import "./helpers/main";
+import "./helpers/products";
+import "./helpers/projects";
